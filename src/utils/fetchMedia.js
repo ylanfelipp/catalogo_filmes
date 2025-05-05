00878cab5cfa17a27d2707ad4b19fb8a7b0d47d2
Ylan <ylanfelipp2@email.com>
2025-05-05 14:28:24 -0300

    bug fix paginação

--- src/utils/fetchMedia.js
+++ src/utils/fetchMedia.js
@@ -3,6 +3,9 @@ const api_url = import.meta.env.VITE_API_URL
 const api_key = import.meta.env.VITE_API_KEY
 
 export async function fetchPopularMovies(page = 1) {
+    if (page < 1) {
+        page = 1
+    }
     const result = await fetch(`${api_url}/movie/popular?api_key=${api_key}&language=pt-BR&page=${page}`)
     if (!result.ok) {
         const err = await result.json().catch(() => ({}));
